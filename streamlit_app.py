a0a89bf762d26c53c74ab0d2e5bd5bf63566fe06
Dawn Kernen <109358187+kernend@users.noreply.github.com>
2023-01-05 11:30:53 -0600

    Update streamlit_app.py
    extra text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Super Healthy Diner')
+
+streamlit.header('Breakfast Super Menu')
+streamlit.text('Omega 3  & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boailed Free-Range Egg')
