6ce10a287d1c3ed108e915e302424789055f99a2
Dawn Kernen <109358187+kernend@users.noreply.github.com>
2023-01-05 11:39:08 -0600

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Super Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Super Menu')
-streamlit.text('Omega 3  & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3  & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avodado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
